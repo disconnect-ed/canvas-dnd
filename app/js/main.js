be050af11235ad5b251eb4b8893621dd1f7b4afd
disconnect-ed <ruslan199812@gmail.com>
2020-09-25 19:03:24 +0300

    added rendering of figures on the canvas, added 'drawFigure' function to define the color and draw the figure

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,7 @@ const cnv = document.querySelector('#canvas')
 const ctx = cnv.getContext('2d')
 const width = 546
 const height = 456
+let cnvCoords = cnv.getBoundingClientRect()
 cnv.width = width
 cnv.height = height
 
@@ -24,38 +25,6 @@ const mouse = {
 // === DATA ===
 
 // === DRAG FIGURES ===
-
-const block = document.querySelector('#block')
-const circle = document.querySelector('#circle')
-
-block.draggable = true
-circle.draggable = true
-
-block.addEventListener('dragend', e => {
-    cursorInCanvas(e)
-    const elemBelow = document.elementFromPoint(e.clientX, e.clientY);
-    if (elemBelow === cnv) {
-        figures.push(new Rect((mouse.x - 25), (mouse.y - 25), 50, 50, 'gold', 'rect'))
-    }
-    console.log(figures)
-})
-
-circle.addEventListener('dragend', e => {
-    cursorInCanvas(e)
-    const elemBelow = document.elementFromPoint(e.clientX, e.clientY);
-    if (elemBelow === cnv) {
-        figures.push(new Circle((mouse.x - 25), (mouse.y - 25)))
-    }
-    console.log(figures)
-})
-
-// === DRAG FIGURES ===
-
-const cursorInCanvas = (e) => {
-    const targetCoords = cnv.getBoundingClientRect();
-    mouse.x = e.pageX - targetCoords.x
-    mouse.y = e.pageY - targetCoords.y
-}
 
 const Rect = function (x, y, w = 50, h = 50, color = 'gold', elemType = 'rect') {
     this.x = x
@@ -77,3 +46,91 @@ const Circle = function (x, y, r = 25, sAngle = 0, eAngle = (2 * Math.PI), clock
     this.color = color
     this.elemType = elemType
 }
+
+Rect.prototype = {
+    draw() {
+        ctx.fillRect(this.x, this.y, this.w, this.h)
+    },
+    stroke() {
+        ctx.strokeRect(this.x, this.y, this.w, this.h)
+    }
+}
+
+Circle.prototype = {
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.r, this.sAngle, this.eAngle, this.clockwise)
+        ctx.fill()
+        ctx.closePath()
+    },
+    stroke() {
+        ctx.stroke()
+    }
+}
+
+// === DRAG FIGURES ===
+
+// === DRAG FIGURES METHODS ===
+
+const block = document.querySelector('#block')
+const circle = document.querySelector('#circle')
+
+block.draggable = true
+circle.draggable = true
+
+const cursorInCanvas = (e) => {
+    const targetCoords = cnv.getBoundingClientRect();
+    mouse.x = e.pageX - targetCoords.x
+    mouse.y = e.pageY - targetCoords.y
+}
+
+block.addEventListener('dragend', e => {
+    cursorInCanvas(e)
+    const elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+    if (elemBelow === cnv) {
+        figures.push(new Rect((mouse.x - 25), (mouse.y - 25), 50, 50, 'gold', 'rect'))
+    }
+    console.log(figures)
+})
+
+circle.addEventListener('dragend', e => {
+    cursorInCanvas(e)
+    const elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+    if (elemBelow === cnv) {
+        figures.push(new Circle((mouse.x - 25), (mouse.y - 25)))
+    }
+    console.log(figures)
+})
+
+// === DRAG FIGURES METHODS ===
+
+// === METHODS ===
+
+const drawFigure = (figure) => {
+    ctx.fillStyle = figure.color
+    figure.draw()
+}
+
+// === METHODS ===
+
+
+// === RENDER ===
+
+setInterval(function (e) {
+    cnvCoords = cnv.getBoundingClientRect()
+    ctx.clearRect(0, 0, width, height)
+    for (let i in figures) {
+        drawFigure(figures[i])
+        // if (figures[i] === selected) {
+        //     figures[i].stroke()
+        // }
+    }
+    // if (drag.elemType === 'rect') {
+    //     rectIsMoving(drag)
+    // }
+    // if (drag.elemType === 'circle') {
+    //     circleIsMoving()
+    // }
+}, 1)
+
+// === RENDER ===
